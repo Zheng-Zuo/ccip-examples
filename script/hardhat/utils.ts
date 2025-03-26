ded346c00635e6a192a0e796496934e471c356e1
“Zheng-Zuo” <zz713@nyu.edu>
2025-03-26 23:42:43 +0800

    change tab width

--- script/hardhat/utils.ts
+++ script/hardhat/utils.ts
@@ -1,18 +1,18 @@
-import { run } from 'hardhat';
+import { run } from "hardhat";
 
 export const verify = async (contractAddress: string, args: any[], path?: string) => {
-    console.log("Verifying contract: ", contractAddress)
+    console.log("Verifying contract: ", contractAddress);
     try {
         await run("verify:verify", {
             address: contractAddress,
             constructorArguments: args,
             contract: path,
-        })
+        });
     } catch (e: any) {
         if (e.message.toLowerCase().includes("already verified")) {
-            console.log("Contract already verified")
+            console.log("Contract already verified");
         } else {
-            console.log(e)
+            console.log(e);
         }
     }
-}
+};
